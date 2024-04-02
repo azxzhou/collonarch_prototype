292c28ae370ccb46b72b54e523103c78c6aca796
azxzhou <azxzhou474@gmail.com>
2024-04-02 09:46:24 -0400

    other files i missed apparently

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
-function love.draw()
+function love.load() --loads all assets at the beginning
+	love.window.setMode(800, 600) --window size
+
+	background = love.graphics.newImage("Assets/background.png") 
+	x = 0
+	y = 0
+
+end
+
+
+function love.draw() --actually draws the assets you loaded, once per frame
 	love.graphics.print("Hello world", 400, 300)
+	love.graphics.draw(background, 0, 0)
+
 end
